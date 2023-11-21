8e22e5143d7d9d3eac6d0dfd630e8ac6592837a0
zeus <zeuskeyz@gmail.com>
2023-11-21 15:45:28 +0300

    changes the border settings

--- src/component/EmojiCard.js
+++ src/component/EmojiCard.js
@@ -5,16 +5,16 @@ import Axios from "axios"
 export const EmojiCard = ({verdict, emoji, expression, id}) => {
 
   let [type, setType] = useState(id)
-  let [styles, setStyles] = useState({backgroundColor: 'black', color: 'white', border: '-2px solid yellow'})
+  let [styles, setStyles] = useState({backgroundColor: 'black', color: 'white', border: '2px solid yellow'})
    
   const handleClick = (event)=>{
     setType(event.target.id)
     Axios.post('https://rate-me-zpzf.onrender.com', {type}).then().catch(err=> {return <Alert variant="success">{err.message}</Alert>}) 
   }
 
-  const handleMouseIn = () => setStyles({backgroundColor: 'transparent', color: 'black', border: '2px solid black'})
+  const handleMouseIn = () => setStyles({backgroundColor: 'transparent', color: 'black', border: '2px solid yellow'})
 
-  const handleMouseOut = () => setStyles({backgroundColor: 'black', color: 'white', border: '2px solid yellow'})
+  const handleMouseOut = () => setStyles({backgroundColor: 'black', color: 'white', border: '2px solid black'})
 
   return (
     <>
